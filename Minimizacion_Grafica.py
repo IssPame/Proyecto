a0caba44590ea36b394e3503673eedb136439fb5
IssPame19 <issabelpamela4e@gmail.com>
2024-04-26 05:52:57 -0400

    Grafica las restricciones
    falta que marque los puntos de interseccion, grafique la funcion objetivo y pinte la region solucion

--- Minimizacion_Grafica.py
+++ Minimizacion_Grafica.py
@@ -52,3 +52,46 @@ for i, (Ec_x, Ec_y, Tipo, ValorEc) in enumerate(restricciones, start=1):
     print(f"Restriccion {i}: {Ec_x}*x1 + {Ec_y}*x2 {Tipo} {ValorEc}")
 print()
 
+# Transformacion de inecuaciones a ecuaciones lineales
+Res_Lineales = []
+for Ec_x, Ec_y, Tipo, ValorEc in restricciones:
+    if Tipo == '<=':
+        ValorEc_lineal = ValorEc
+    elif Tipo == '<':
+        ValorEc_lineal = ValorEc
+    elif Tipo == '>':
+        ValorEc_lineal = ValorEc
+    elif Tipo == '>=':
+        ValorEc_lineal = ValorEc
+    else: # Tipo == '='
+        ValorEc_lineal = ValorEc
+    Res_Lineales.append((Ec_x, Ec_y, '=', ValorEc_lineal))
+
+# Definir el rango de valores para x y y
+x_vals = np.linspace(0, 2000, 200)
+y_vals = np.linspace(0, 2000, 200)
+
+# Crear una figura y un conjunto de ejes
+fig, ax = plt.subplots()
+
+# Definir colores para las líneas
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k'] # Lista de colores para las líneas
+
+ax.set_xlim(left=0, right=2000)
+ax.set_ylim(bottom=0, top=2000)
+
+# Para cada ecuación lineal, calcular y trazar la línea
+for i, (Ec_x, Ec_y, Tipo, ValorEc) in enumerate(Res_Lineales):
+    # Calcular los valores de y para cada valor de x
+    y_line = (ValorEc - Ec_x * x_vals) / Ec_y
+    # Trazar la línea con un color diferente
+    ax.plot(x_vals, y_line, color=colors[i % len(colors)], label= restricciones[i])
+
+# Configurar el gráfico
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Gráfico de las restricciones')
+ax.legend() # Mostrar la leyenda
+
+# Mostrar el gráfico
+plt.show()
