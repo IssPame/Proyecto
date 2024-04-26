b5e7f5788fe8810e64d8f9439e2a1bde6680ae04
IssPame19 <issabelpamela4e@gmail.com>
2024-04-25 22:34:54 -0400

    Agrego manejo de errores

--- Minimizacion_Grafica.py
+++ Minimizacion_Grafica.py
@@ -8,33 +8,43 @@ x, y = sp.symbols('x, y')
 
 # Funcion para validar el operador de la inecuacion
 # Raise interrumpe el programa y activa el manejo de excepciones
-def Operadores(op):
+def Validar_Op(op):
     Op_Validos = ['<=', '<', '>', '>=']
     if op not in Op_Validos:
         raise ValueError(f"Ingrese un operador valido {Op_Validos}")
     return op
 
+# Funcion para validar numeros
+def Validar_Num(msj):
+    while True:
+        try:
+            num = float(input(msj))
+            return num
+        except ValueError:
+            print("Por favor ingrese un numero valido")
+
 # Solicitud de datos de la funcion objetivo
-F_Obj_x = float(input("Indique el valor de X1 en la funcion objetivo: "))
-F_Obj_y = float(input("Indique el valor de X2 en la funcion objetivo: "))
+F_Obj_x = Validar_Num("Indique el valor de X1 en la funcion objetivo: ")
+F_Obj_y = Validar_Num("Indique el valor de X2 en la funcion objetivo: ")
 Z = F_Obj_x*x + F_Obj_y*y
 print("La función objetivo es: Z = ", Z)
 print()
 
 # Solicitud de datos de las restricciones
-Restricciones = int(input("Indique el numero de restricciones: "))
+Restricciones = Validar_Num("Indique el numero de restricciones: ")
+Restricciones = int(Restricciones)
 restricciones = []
 
 for i in range(Restricciones):
     print()
-    Ec_x = float(input(f"Indique el valor de X1 en la restriccion {i+1}: "))
-    Ec_y = float(input(f"Indique el valor de X2 en la restriccion {i+1}: "))
+    Ec_x = Validar_Num(f"Indique el valor de X1 en la restriccion {i+1}: ")
+    Ec_y = Validar_Num(f"Indique el valor de X2 en la restriccion {i+1}: ")
     Tipo = input("Indique el tipo de restriccion (<=, <, >, >=): ")
     while Tipo not in ['<=', '<', '>', '>=']:
         print("Operador inválido. Intente de nuevo.")
         Tipo = input("Indique el tipo de restricción (<=, <, >, >=): ")
-    Tipo = Operadores(Tipo)
-    ValorEc = float(input(f"Indique el valor de la restriccion {i+1}: "))
+    Tipo = Validar_Op(Tipo)
+    ValorEc = Validar_Num(f"Indique el valor de la restriccion {i+1}: ")
     restricciones.append((Ec_x, Ec_y, Tipo, ValorEc))
 print()
 
